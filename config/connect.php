05bc81699559b6e7b84bf9e6d412efc75d5375c1
alleidda <aaddiella@gmail.com.com>
2022-11-24 07:38:29 +0100

    changing connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'host'    => 'localhost',
-    'dbname'  => 'othf1415_blog_p5',
-    'username'=> 'othf1415_othf1415',
-    'password'=> '6xgLYaYMturj'
+    'dbname'  => '',
+    'username'=> '',
+    'password'=> ''
 ];
