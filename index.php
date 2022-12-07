1d0d4dbebdd9e00fbda0ac0c1b1cd0ee61d62787
alleidda <aaddiella@gmail.com.com>
2022-12-07 23:07:12 +0100

    codacy

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
-require_once ('controller/frontend.php');
+require_once('controller/frontend.php');
 
 use \Addiella\controller\Frontend;
 use \Addiella\model\Session;
 //Autoload
-require_once ('vendor/autoload.php');
+require_once('vendor/autoload.php');
 
 
 $session = new Session();
 $session->start();
 
 try {
+
     if (isset($_GET['action'])) {
- if ($_GET['action'] == 'readPost') {
+        $action = $_GET['action'];
+        if (($action == 'readPost')) {
             if (isset($_GET['id']) && $_GET['id'] > 0 && filter_var($_GET['id'], FILTER_VALIDATE_INT) !== false) {
                 $idPost = $_GET['id'];
                 $frontend = new Frontend();
@@ -21,63 +23,58 @@ try {
             }
             throw new \Exception('Aucun identifiant de billet envoyé');
             return;
+        } elseif ($action == 'postsList') {
+            $frontend = new Frontend();
+            $frontend->getPosts();
+            return;
+        } elseif ($action == 'forgotPasswordView') {
+            $frontend = new Frontend();
+            $frontend->forgotPasswordView();
+            return;
+        } elseif ($action == 'resetPasswordView') {
+            $frontend = new Frontend();
+            $frontend->resetPasswordView();
+            return;
+        } elseif ($action == 'resetPassword') {
+            $frontend = new Frontend();
+            $frontend->resetPassword();
+            return;
+        } elseif ($action == 'signUpView') {
+            $frontend = new Frontend();
+            $frontend->signUpView();
+            return;
+        } elseif ($action == 'signInView') {
+            $frontend = new Frontend();
+            $frontend->signInView();
+            return;
         }
- 
- elseif ($_GET['action'] == 'postsList') {
-                $frontend = new Frontend();
-                $frontend->getPosts();
-                return;
-        } elseif ($_GET['action'] == 'forgotPasswordView') {
-                $frontend = new Frontend();
-                $frontend->forgotPasswordView();
-                return;
-        } elseif ($_GET['action'] == 'forgotPassword') {
-                $frontend = new Frontend();
-                $frontend->forgotPassword();
-                return;
-        } elseif ($_GET['action'] == 'resetPasswordView') {
-                $frontend = new Frontend();
-                $frontend->resetPasswordView();
-                return;
-        } elseif ($_GET['action'] == 'resetPassword') {
-                $frontend = new Frontend();
-                $frontend->resetPassword();
-                return;
-        } elseif ($_GET['action'] == 'signUpView') {
-                $frontend = new Frontend();
-                $frontend->signUpView();
-                return;
-        } elseif ($_GET['action'] == 'signInView') {
-                $frontend = new Frontend();
-                $frontend->signInView();
-                return;
-        } if ($_GET['action'] == 'signUp') {
+        if ($action == 'signUp') {
             // On check que le formulaire a été envoyé
             if (isset($_POST['email'])) {
                 $frontend = new Frontend();
                 $frontend->signUp();
                 return;
             }
-        } elseif ($_GET['action'] == 'signIn') {
+        } elseif ($action == 'signIn') {
             // On check que le formulaire a été envoyé
             if (isset($_POST['email'])) {
                 $frontend = new Frontend();
                 $frontend->signIn();
                 return;
             }
-        }  elseif ($_GET['action'] == 'homeView') {
+        } elseif ($action == 'homeView') {
             $frontend = new Frontend();
             $frontend->homeView();
             return;
-        } elseif ($_GET['action'] == 'contactView') {
+        } elseif ($action == 'contactView') {
             $frontend = new Frontend();
             $frontend->contactView();
             return;
-        } elseif ($_GET['action'] == 'postsListView') {
+        } elseif ($action == 'postsListView') {
             $frontend = new Frontend();
             $frontend->getPosts();
             return;
-        } elseif ($_GET['action'] == 'cvView') {
+        } elseif ($action == 'cvView') {
             $frontend = new Frontend();
             $frontend->cvView();
             return;
